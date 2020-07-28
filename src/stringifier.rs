16cc2f2c524b73cb54c3072c88034574cb23f55b
むずでょ <muzudho1@gmail.com>
2020-07-29 01:28:59 +0900

    改行調査中。

--- src/stringifier.rs
+++ src/stringifier.rs
@@ -24,6 +24,13 @@ const NEW_LINE_SEQUENCE: &'static str = "\\n";
 #[cfg(not(windows))]
 const NEW_LINE_CHARS: &'static [char; 1] = &['\n'];
 
+enum NewLineType {
+    /// `\r\n`.
+    Windows,
+    /// `\n`
+    NotWindows,
+}
+
 /// Unstable.  
 /// Escape control characters.  
 /// 仕様は変わることがあります。  
@@ -63,7 +70,21 @@ impl Stringifier {
         // D) You must use one-line ['''].
         // E) You must use ['].
         // F) Use ["].
-        if 1 < value.lines().count() {
+        let multi_line = if 1 < value.lines().count() {
+            true
+        } else {
+            /*
+            // "'xxx'\r\n" Supports missing cases.
+            // "'xxx'\r\n" ケースの取り逃しに対応。
+            let ch_vec: Vec<char> = value.chars().collect();
+            if let Some(_) = Stringifier::which_new_line_type(&ch_vec) {
+                true
+            } else {
+            */
+            false
+            //}
+        };
+        if multi_line {
             // Multi-line string.
             // if let Ok(re) = RE_TRIPLE_SINGLE_QUOTE.lock() {
             // if re.is_match(value) {
@@ -75,6 +96,7 @@ impl Stringifier {
                 } else {
                     Stringifier::escape_double_quotation(value)
                 };
+                // (Result 3) Triple double quoted, Multi-line.
                 return format!(
                     "\"\"\"
 {}
@@ -82,6 +104,7 @@ impl Stringifier {
                     escaped_string
                 );
             }
+            // (Result 6) Triple single quoted, Multi-line.
             format!(
                 "'''
 {}
@@ -92,18 +115,21 @@ impl Stringifier {
             // One liner.
             let escaped_trailng_newline_string = Stringifier::escape_trailing_newline(value);
             if let Some(escaped_trailng_newline_string) = escaped_trailng_newline_string {
+                // (Result 1) Double quoted, Single-line.
                 return format!(
                     "\"{}\"",
                     Stringifier::escape_double_quotation(&escaped_trailng_newline_string)
                 );
             }
             if value.contains("'") {
+                // (Result 1) Double quoted, Single-line.
                 return format!(
                     "\"{}\"",
                     Stringifier::escape_double_quotation(&Stringifier::escape_back_slash(value))
                 );
             }
 
+            // (Result 4) Single quoted, Single-line.
             format!("'{}'", value)
         }
     }
@@ -118,6 +144,30 @@ impl Stringifier {
     pub fn escape_double_quotation(text: &str) -> String {
         text.replace("\"", "\\\"")
     }
+    fn which_new_line_type(ch_vec: &Vec<char>) -> Option<NewLineType> {
+        if NEW_LINE_CHARS.len() == 2 && 1 < ch_vec.len() {
+            if ch_vec[ch_vec.len() - 2] == NEW_LINE_CHARS[0]
+                && ch_vec[ch_vec.len() - 1] == NEW_LINE_CHARS[1]
+            {
+                // For windows.
+                Some(NewLineType::Windows)
+            } else {
+                // No trailing new line.
+                None
+            }
+        } else if NEW_LINE_CHARS.len() == 1 && 0 < ch_vec.len() {
+            if ch_vec[ch_vec.len() - 1] == NEW_LINE_CHARS[0] {
+                // TODO For linux OS.
+                Some(NewLineType::NotWindows)
+            } else {
+                // No trailing new line.
+                None
+            }
+        } else {
+            // No trailing new line.
+            None
+        }
+    }
     /// Escape trailing newline.  
     /// 末尾の改行をエスケープします。  
     ///
@@ -127,54 +177,47 @@ impl Stringifier {
     /// エスケープした文字列か、あるいは None です。  
     pub fn escape_trailing_newline(value: &str) -> Option<String> {
         let ch_vec: Vec<char> = value.chars().collect();
-        if NEW_LINE_CHARS.len() == 2 && 1 < ch_vec.len() {
-            if ch_vec[ch_vec.len() - 2] == NEW_LINE_CHARS[0]
-                && ch_vec[ch_vec.len() - 1] == NEW_LINE_CHARS[1]
-            {
-                // TODO For windows.
-                //*
-                Some(format!("{}{}", value.trim_end(), NEW_LINE_SEQUENCE).to_string())
-            // */
-            /*
-                // Remove new line code.
-                ch_vec.pop();
-                ch_vec.pop();
-                // Append escaped new line.
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[0]);
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[1]);
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[2]);
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[3]);
-                // From vector to string.
-                ch_vec.iter().map(|&s| s as char).collect::<String>()
-            // */
-            /*
-            value.to_string()
-            // */
-            } else {
-                // Don't.
-                None
-            }
-        } else if NEW_LINE_CHARS.len() == 1 && 0 < ch_vec.len() {
-            if ch_vec[ch_vec.len() - 1] == NEW_LINE_CHARS[0] {
-                // TODO For linux OS.
-                //*
-                Some(format!("{}{}", value.trim_end(), NEW_LINE_SEQUENCE).to_string())
-            // */
-            /*
-                // Remove new line code.
-                ch_vec.pop();
-                // Append escaped new line.
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[0]);
-                ch_vec.push(NEW_LINE_ESCAPED_CHARS[1]);
-                // From vector to string.
-                ch_vec.iter().map(|&s| s as char).collect::<String>()
-            // */
-            /*
-            value.to_string()
-            // */
-            } else {
-                // Don't.
-                None
+        if let Some(t) = Stringifier::which_new_line_type(&ch_vec) {
+            match t {
+                NewLineType::NotWindows => {
+                    // For Unix OS.
+                    //*
+                    Some(format!("{}{}", value.trim_end(), NEW_LINE_SEQUENCE).to_string())
+                    // */
+                    /*
+                        // Remove new line code.
+                        ch_vec.pop();
+                        // Append escaped new line.
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[0]);
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[1]);
+                        // From vector to string.
+                        ch_vec.iter().map(|&s| s as char).collect::<String>()
+                    // */
+                    /*
+                    value.to_string()
+                    // */
+                }
+                NewLineType::Windows => {
+                    // For windows.
+                    //*
+                    Some(format!("{}{}", value.trim_end(), NEW_LINE_SEQUENCE).to_string())
+                    // */
+                    /*
+                        // Remove new line code.
+                        ch_vec.pop();
+                        ch_vec.pop();
+                        // Append escaped new line.
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[0]);
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[1]);
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[2]);
+                        ch_vec.push(NEW_LINE_ESCAPED_CHARS[3]);
+                        // From vector to string.
+                        ch_vec.iter().map(|&s| s as char).collect::<String>()
+                    // */
+                    /*
+                    value.to_string()
+                    // */
+                }
             }
         } else {
             // No trailing new line.
